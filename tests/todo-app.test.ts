810f734a2aa5bcec117f95e866cd5b2ba54fba52
Rita Castro <ritamcastro@gmail.com>
2026-04-03 15:53:17 +0100

    🚧 Trying to write the first test

---
+++ tests/todo-app.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('I want to add a new To-Do', async ({ page }) => {
+  // Visit the homepage
+  await page.goto('/');
+
+  // Click something to add the new item
+  // ??????
+
+  // The item is added to the page
+  // ??????
+
+});
+
