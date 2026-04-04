054bbde5375483a5ccc4c942ed86b64e34629b60
Rita Castro <ritamcastro@gmail.com>
2026-04-04 15:45:50 +0100

    🚧  Add test with proper roles and semantics

--- tests/todo-app.test.ts
+++ tests/todo-app.test.ts
@@ -4,9 +4,27 @@ test('I want to add a new To-Do', async ({ page }) => {
   // Visit the homepage
   await page.goto('/')
 
-  // Click something to add the new item
-  await page.getByText('New').click()
+  // Click _on a button_ to add the new item
+  await page.getByRole('button', { name: 'New' }).click()
 
   // The item is added to the page
-  await expect(page.getByText('What needs to be done?')).toBeVisible()
+  // ... and will be able to receive text
+  await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible()
 })
+
+// import { expect, test } from '@playwright/test'
+
+// test('I want to add a new To-Do', async ({ page }) => {
+//   // Visit the homepage
+//   await page.goto('/')
+
+//   // Click _on a button_ to add the new item
+//   await page.getByRole('button', { name: 'New' }).click()
+
+//   // The item is added to the page...
+//   // ... and is unchecked
+//   await expect(page.getByRole('checkbox')).not.toBeChecked()
+
+//   // ... and will be able to receive text
+//   await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible()
+// })
