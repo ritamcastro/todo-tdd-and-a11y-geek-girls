96c9e418b9de9e65635357a1316a6949cfb8b97f
Rita Castro <ritamcastro@gmail.com>
2026-04-04 19:11:19 +0100

    🧪  Add tests for done todos to be at the end of the list

--- tests/todo-app.test.ts
+++ tests/todo-app.test.ts
@@ -1,3 +1,4 @@
+import { todo } from 'node:test'
 import { expect, test } from '@playwright/test'
 
 test.describe('The To-Do application', () => {
@@ -33,5 +34,31 @@ test.describe('The To-Do application', () => {
     await expect(page.getByRole('checkbox')).toBeChecked()
     await expect(page.getByRole('deletion')).toBeVisible()
     await expect(page.getByText('Prepare the workshop')).toBeVisible()
+  })
+
+  test('I want to move "done" To-Dos to the end of the list', async ({ page }) => {
+    await page.goto('/')
+
+    const newToDoButton = page.getByRole('button', { name: 'New' })
+    const toDoList = page.getByRole('list')
+
+    await toDoList.getByPlaceholder('What needs to be done?').fill('This is the first item')
+
+    await newToDoButton.click()
+    await newToDoButton.click()
+
+    const toDoItems = toDoList.getByRole('listitem')
+    await expect(toDoItems).toHaveCount(3)
+
+    await toDoList.getByPlaceholder('What needs to be done?').nth(1).fill('This is the second item')
+    await toDoList.getByPlaceholder('What needs to be done?').nth(2).fill('This is the third item')
+
+    const itemToMarkAsDone = toDoList.getByRole('checkbox').nth(1)
+    await itemToMarkAsDone.check()
+
+    await expect(toDoItems.getByRole('textbox')).toHaveCount(2)
+    await expect(toDoItems.getByRole('deletion')).toHaveCount(1)
+
+    await expect(toDoItems.last().getByRole('deletion')).toBeVisible()
   })
 })
