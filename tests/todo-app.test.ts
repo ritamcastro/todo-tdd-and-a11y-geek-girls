fd4909eb98806c564ea5d6800fff99e6e27c097e
Rita Castro <ritamcastro@gmail.com>
2026-04-04 17:32:24 +0100

    🧪  Crossing off a to-do and seeing it via css

--- tests/todo-app.test.ts
+++ tests/todo-app.test.ts
@@ -6,11 +6,26 @@ test('I want to add a new To-Do', async ({ page }) => {
 
   // There is already a to-do in the page
   await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible()
+  await expect(page.getByRole('checkbox')).not.toBeChecked()
 
   // Click _on a button_ to add the new item
   await page.getByRole('button', { name: 'New' }).click()
 
   // The item is added to the page
-  // ... and will be able to receive text
   await expect(page.getByPlaceholder('What needs to be done?')).toHaveCount(2)
+  await expect(page.getByRole('checkbox')).toHaveCount(2)
+})
+
+test('I want to cross off a To-Do when it is done', async ({ page }) => {
+  await page.goto('/')
+
+  const todoText = page.getByPlaceholder('What needs to be done?')
+
+  await todoText.fill('Prepare the workshop')
+  await expect(todoText).toHaveCSS('text-decoration', /none/)
+
+  await page.getByRole('checkbox').click()
+
+  await expect(page.getByRole('checkbox')).toBeChecked()
+  await expect(todoText).toHaveCSS('text-decoration', /line-through/)
 })
