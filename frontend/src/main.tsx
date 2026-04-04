bf1e04c54126ffbc98ac5fe33f36f0dfe08eabbe
Rita Castro <ritamcastro@gmail.com>
2026-04-04 17:32:56 +0100

    ✅  Making the test pass

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,26 +6,41 @@ const rootContainer: Element | DocumentFragment | null = document.getElementById
 type ToDoItem = {
   id: number
   text: string
+  isDone: boolean
 }
 
 const ToDo = () => {
-  const [items, setItems] = useState<ToDoItem[]>([{ id: Date.now(), text: '' }])
+  const [items, setItems] = useState<ToDoItem[]>([{ id: Date.now(), text: '', isDone: false }])
+
+  const onToggleItem = (id: number) => {
+    const updatedItems = items.map(item => {
+      return item.id === id ? { ...item, isDone: !item.isDone } : item
+    })
+    setItems(updatedItems)
+  }
 
   return (
     <>
       <button
         type="button"
-        onClick={() => setItems([...items, { id: Date.now(), text: '' }])}
+        onClick={() => setItems([...items, { id: Date.now(), text: '', isDone: false }])}
       >
         New
       </button>
       <div>
         {items.map(item => (
-          <input
-            key={item.id}
-            type="text"
-            placeholder="What needs to be done?"
-          />
+          <div key={item.id}>
+            <input
+              type={'checkbox'}
+              checked={item.isDone}
+              onChange={() => onToggleItem(item.id)}
+            />
+            <input
+              type="text"
+              placeholder="What needs to be done?"
+              style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}
+            />
+          </div>
         ))}
       </div>
     </>
