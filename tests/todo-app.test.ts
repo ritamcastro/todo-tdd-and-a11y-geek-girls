159bd2fe0fe548031e13ee60570057640eef3c2e
Rita Castro <ritamcastro@gmail.com>
2026-04-04 18:22:40 +0100

    🧪 Removing css as the driver for knowing that a todo is done

--- tests/todo-app.test.ts
+++ tests/todo-app.test.ts
@@ -1,31 +1,37 @@
 import { expect, test } from '@playwright/test'
 
-test('I want to add a new To-Do', async ({ page }) => {
-  // Visit the homepage
-  await page.goto('/')
+test.describe('The To-Do application', () => {
+  test('I want to add a new To-Do', async ({ page }) => {
+    // Visit the homepage
+    await page.goto('/')
 
-  // There is already a to-do in the page
-  await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible()
-  await expect(page.getByRole('checkbox')).not.toBeChecked()
+    // There is already a to-do in the page
+    await expect(page.getByPlaceholder('What needs to be done?')).toBeVisible()
+    await expect(page.getByRole('checkbox')).not.toBeChecked()
 
-  // Click _on a button_ to add the new item
-  await page.getByRole('button', { name: 'New' }).click()
+    // Click _on a button_ to add the new item
+    await page.getByRole('button', { name: 'New' }).click()
 
-  // The item is added to the page
-  await expect(page.getByPlaceholder('What needs to be done?')).toHaveCount(2)
-  await expect(page.getByRole('checkbox')).toHaveCount(2)
-})
+    // The item is added to the page
+    await expect(page.getByPlaceholder('What needs to be done?')).toHaveCount(2)
+    await expect(page.getByRole('checkbox')).toHaveCount(2)
+  })
 
-test('I want to cross off a To-Do when it is done', async ({ page }) => {
-  await page.goto('/')
+  test('I want to cross off a To-Do when it is done', async ({ page }) => {
+    await page.goto('/')
 
-  const todoText = page.getByPlaceholder('What needs to be done?')
+    const todoText = page.getByPlaceholder('What needs to be done?')
 
-  await todoText.fill('Prepare the workshop')
-  await expect(todoText).toHaveCSS('text-decoration', /none/)
+    await todoText.fill('Prepare the workshop')
+    await todoText.blur()
 
-  await page.getByRole('checkbox').click()
+    await expect(page.getByRole('deletion')).not.toBeVisible()
+    await expect(todoText).toHaveValue('Prepare the workshop')
 
-  await expect(page.getByRole('checkbox')).toBeChecked()
-  await expect(todoText).toHaveCSS('text-decoration', /line-through/)
+    await page.getByRole('checkbox').click()
+
+    await expect(page.getByRole('checkbox')).toBeChecked()
+    await expect(page.getByRole('deletion')).toBeVisible()
+    await expect(page.getByText('Prepare the workshop')).toBeVisible()
+  })
 })
