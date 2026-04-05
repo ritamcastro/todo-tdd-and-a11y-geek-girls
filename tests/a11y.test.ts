8bb99d0bb6834045075ee0a8e5c9781d5af10528
Rita Castro <ritamcastro@gmail.com>
2026-04-05 19:40:29 +0100

    ♿️ Add a11y testing in playwright

---
+++ tests/a11y.test.ts
@@ -0,0 +1,21 @@
+import AxeBuilder from '@axe-core/playwright'
+import { expect, test } from '@playwright/test'
+
+test.describe('To-Do application', () => {
+  test('should not have any automatically detectable accessibility issues', async ({ page }, testInfo) => {
+    await page.goto('/')
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      // Tags: allow you to scan ONLY for a certain level of standards:
+      // https://www.deque.com/axe/core-documentation/api-documentation/?utm_campaign=api-documentation#api-notes
+      // .withTags(['best-practice'])
+      .analyze()
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    })
+
+    expect(accessibilityScanResults.violations).toEqual([])
+  })
+})
